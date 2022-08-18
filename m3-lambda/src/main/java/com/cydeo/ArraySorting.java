496d6e2a9ec20e337e67a9ec1666e753113a49df
violettaqueen <violetta.v.queen@gmail.com>
2022-08-18 15:47:48 -0600

    Lambda

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -2,11 +2,12 @@ package com.cydeo;
 import  java.lang.reflect.Array;
 import  java.util.function.Predicate;
 
-public class ArraySorting {  // QuickSort, BubbleSort
+public class ArraySorting {  // QuickSort, BubbleSort // 1. create class
 
     public static void main(String[] args) {
 
-        ArraySorting as = new ArraySorting();
+        ArraySorting as = new ArraySorting(); // create an object
+        as.sort(); //
 
         QuickSort qs = new QuickSort();
         BubbleSort bs = new BubbleSort();
@@ -18,7 +19,7 @@ public class ArraySorting {  // QuickSort, BubbleSort
         as.sort(() -> System.out.println("Quick Sorting"));
 
     }
-    private void sort(Sorting sorting){
-        sorting.sort();
+    private void sort(){  //create the method
+        System.out.println("Quick Sorting array");;
     }
 }

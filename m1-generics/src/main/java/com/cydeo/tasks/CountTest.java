c9d6108fde661019fe1d015ae4eb5cc35964a928
violettaqueen <violetta.v.queen@gmail.com>
2022-08-17 19:35:51 -0600

    Generics

--- m1-generics/src/main/java/com/cydeo/tasks/CountTest.java
+++ m1-generics/src/main/java/com/cydeo/tasks/CountTest.java
@@ -1,13 +1,30 @@
 package com.cydeo.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountTest {
 
     public static void main(String[] args) {
 
+       List<Integer> list = new ArrayList<>();
+       int count = countIf(list,new OddPredicate());
+        System.out.println("Number of odd integers = " + count);
 
 
 
 
+    }
+    private static <T> int countIf(List<T> c, UnaryPredicate<T> p){
+
+        int count = 0;
+
+        for (T each : c) {
+            if(p.test(each)){
+                count++;
+            }
+        }
+        return count;
     }
 
 

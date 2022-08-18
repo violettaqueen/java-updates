c478f6bbdfc680fb57011e15eb19e0fdc1719850
violettaqueen <violetta.v.queen@gmail.com>
2022-08-17 19:18:06 -0600

    Generics

---
+++ m1-generics/src/main/java/com/cydeo/tasks/CountTest.java
@@ -0,0 +1,14 @@
+package com.cydeo.tasks;
+
+public class CountTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+}
---
+++ m1-generics/src/main/java/com/cydeo/tasks/UnaryPredicate.java
@@ -0,0 +1,6 @@
+package com.cydeo.tasks;
+
+public interface UnaryPredicate<T> {
+
+    public boolean test(T obj);
+}

2fd0e1e9a017bcaf66f142d819ce689e6142ba7d
violettaqueen <violetta.v.queen@gmail.com>
2022-08-18 13:52:07 -0600

    Stream

--- m6-stream/src/main/java/com/cydeo/Main.java
+++ m6-stream/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -15,6 +16,15 @@ public class Main {
         // creating stream from Collection
         List<String> courseList = Arrays.asList("Java", "Spring", "Agile");
         Stream <String> courseStream2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java",100),
+                new Course("DS",101),
+                new Course("MS",102)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
 
 
 

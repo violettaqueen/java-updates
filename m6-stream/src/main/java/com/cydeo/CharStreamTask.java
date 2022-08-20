51400e57b225487f8b5d027ffb185f1a6f0d26b8
violettaqueen <violetta.v.queen@gmail.com>
2022-08-20 11:22:45 -0600

    Stream

---
+++ m6-stream/src/main/java/com/cydeo/CharStreamTask.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class CharStreamTask {
+
+    List<String> words = new ArrayList<>(Arrays.asList("JAVA", "APPLE", "HONDA", "DEVELOPER"));
+
+
+}
